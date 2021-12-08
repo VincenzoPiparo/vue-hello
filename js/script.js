bbea406d00ec275e9b9d6db36ead9c2b919f3307
VincenzoPiparo <v.pipar.97@gmai.com>
2021-12-08 18:11:58 +0100

    END VUE

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 Vue.config.devtools = true;
 
-
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// BONUS:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
 const app = new Vue(
     {
         el: '#root',
